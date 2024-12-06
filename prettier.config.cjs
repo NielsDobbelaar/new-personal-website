a3c7a26f5d92d76295931283827b0c18cb3f8dab
Niels Dobbelaar <s1127720@student.hsleiden.nl>
2024-12-06 14:20:06 +0100

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+import tailwindcss from "prettier-plugin-tailwindcss";
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  endOfLine: "lf",
+  plugins: [tailwindcss],
+};
